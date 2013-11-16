78566d2114262dd8a649b65e99eb4a7d1fda6ff5
Michael Prinsloo <mike.prinsloo@gmail.com>
2013-11-16 23:25:42 +0200

    Added dispersion_plot and generate.

---
+++ ch01/05-dispersion_plot.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python2.7
+
+from nltk.book import *
+
+text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
---
+++ ch01/06-generate.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2.7
+
+from nltk.book import *
+
+# Generate random text in a given style.
+
+text4.generate()
